3387c412b4122e02e1835d17ca512ad58261145a
erfan <erfanabdollahi.dev.com>
2025-12-14 18:51:04 +0000

    cocktails price section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 
 import Navbar from './components/navbar';
 import Hero from './components/Hero';
+import Cocktails from './components/Cocktails';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 
@@ -14,6 +15,7 @@ const App = () => {
         <main>
             <Navbar />
             <Hero/>
+            <Cocktails/>
           
         </main>
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -121,7 +121,7 @@ const Hero = () => {
                     </div>
                 </div>
             </section>
-            <div className="video absolute inset-0">
+            <div className="video  absolute inset-0">
                 <video
                     ref={videoRef}
                     src="/videos/output.mp4"
